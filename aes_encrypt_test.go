d813140e798f4eecc9f4e5eba7946a0666792f39
FanOne <294350394@qq.com>
2022-10-11 23:01:04 +0800

    feat:readme

--- aes_encrypt_test.go
+++ aes_encrypt_test.go
@@ -9,8 +9,8 @@ func TestAesSecret(t *testing.T) {
 	specialSign := "][;,[2psldp0981zx;./"
 	key := "458796" // key 密钥
 	aesEncrypt, _ := NewAesEncrypt(specialSign, key)
-	ans := aesEncrypt.SecretEncrypt("this is a secret", 12)
+	str := aesEncrypt.SecretEncrypt("this is a secret", 12)
+	fmt.Println(str)
+	ans := aesEncrypt.SecretDecrypt(str, 12)
 	fmt.Println(ans)
-	a := aesEncrypt.SecretDecrypt(ans, 12)
-	fmt.Println(a)
 }
